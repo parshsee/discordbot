90b8603bd26ee2292572cb527008724a1a688eca
Parshotan Seenanan <parshsee@gmail.com>
2020-06-10 10:50:49 -0400

    Added API call and message send, added checks for nsfw or spoiler content

--- commands/meme.js
+++ commands/meme.js
@@ -1,6 +1,20 @@
 require('dotenv').config();
 const axios = require('axios');
 
+async function apiCalls() {
+	// Make API call to get image of the random animal
+	// .data to only return the data (response)
+	const memeLink = (await axios({
+		url: process.env.MEME_API,
+		method: 'GET',
+		headers: {
+			'Accept': 'application/json',
+		},
+	})).data;
+
+	return memeLink;
+}
+
 module.exports = {
 	name: 'meme',
 	aliases: [],
@@ -8,7 +22,16 @@ module.exports = {
 	args: false,
 	usage: '',
 	async execute(message, args) {
-		console.log(message, args);
 
+		if(message.channel.name !== 'memes') return message.channel.send('This command can only be used in memes channel');
+
+		const response = await apiCalls();
+
+		return message.channel.send((response.nsfw || response.spoiler) ? `__**NSFW or SPOILER**__ \n${response.title}` : `${response.title}`, {
+			files: [{
+				attachment: `${response.url}`,
+				name: (response.nsfw || response.spoiler) ? 'SPOILER_IMG.jpg' : 'REG_IMG.jpg',
+			}],
+		});
 	},
 };
