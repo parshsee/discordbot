2b8ab479cb86afe75f330160830d97ef921504ba
Parshotan Seenanan <parshsee@gmail.com>
2020-06-11 10:56:34 -0400

    Updated description/usage, added error handling, added functionality for specific subreddit

--- commands/meme.js
+++ commands/meme.js
@@ -1,31 +1,40 @@
 require('dotenv').config();
 const axios = require('axios');
 
-async function apiCalls() {
-	// Make API call to get image of the random animal
+async function apiCalls(subreddit = '') {
+	// Make API call to get random meme from reddit
 	// .data to only return the data (response)
-	const memeLink = (await axios({
-		url: process.env.MEME_API,
-		method: 'GET',
-		headers: {
-			'Accept': 'application/json',
-		},
-	})).data;
+	try {
+		const memeLink = (await axios({
+			url: process.env.MEME_API + `/${subreddit}`,
+			method: 'GET',
+			headers: {
+				'Accept': 'application/json',
+			},
+		}));
 
-	return memeLink;
+		return memeLink.data;
+	} catch (error) {
+		console.log(error.response);
+		return error.response.data;
+	}
+
 }
 
 module.exports = {
 	name: 'meme',
 	aliases: [],
-	description: 'Sends a random meme!',
+	description: 'Sends a meme from a random subreddit (dankmemes, memes, me_irl) or a specified one!',
 	args: false,
-	usage: '',
+	usage: ' **OR** \nia!meme [subreddit name]',
 	async execute(message, args) {
 
 		if(message.channel.name !== 'memes') return message.channel.send('This command can only be used in memes channel');
 
-		const response = await apiCalls();
+		const subreddit = args.join(' ');
+		const response = await apiCalls(subreddit);
+
+		if(response.code) return message.channel.send('This subreddit does not exist, is typed incorrectly, or has no images.');
 
 		return message.channel.send((response.nsfw || response.spoiler) ? `__**NSFW or SPOILER**__ \n${response.title}` : `${response.title}`, {
 			files: [{
