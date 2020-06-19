f296a79411ba39143b653a4c12e020f32c592961
Parshotan Seenanan <parshsee@gmail.com>
2020-06-19 09:41:10 -0400

    Added comments and aliases

--- commands/meme.js
+++ commands/meme.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 const axios = require('axios');
 
+// Function to make call to meme API
 async function apiCalls(subreddit = '') {
 	// Make API call to get random meme from reddit
 	// .data to only return the data (response)
@@ -15,6 +16,7 @@ async function apiCalls(subreddit = '') {
 
 		return memeLink.data;
 	} catch (error) {
+		// Log the API error and return the data
 		console.log(error.response);
 		return error.response.data;
 	}
@@ -23,19 +25,25 @@ async function apiCalls(subreddit = '') {
 
 module.exports = {
 	name: 'meme',
-	aliases: [],
+	aliases: ['memes'],
 	description: 'Sends a meme from a random subreddit (dankmemes, memes, me_irl) or a specified one!',
 	args: false,
 	usage: ' **OR** \nia!meme [subreddit name]',
 	async execute(message, args) {
 
+		// If channel isn't memes channel return error message
+		// Remove to allow memes in any channel
 		if(message.channel.name !== 'memes') return message.channel.send('This command can only be used in memes channel');
 
+		// Get the subreddit name from the args
+		// Get response from meme api call
 		const subreddit = args.join(' ');
 		const response = await apiCalls(subreddit);
 
+		// If given a response code (error) return error message
 		if(response.code) return message.channel.send('This subreddit does not exist, is typed incorrectly, or has no images.');
 
+		// Send the image from the response with ternary checks to see if its NSFW or Spoiler and blurring image if true
 		return message.channel.send((response.nsfw || response.spoiler) ? `__**NSFW or SPOILER**__ \n${response.title}` : `${response.title}`, {
 			files: [{
 				attachment: `${response.url}`,
