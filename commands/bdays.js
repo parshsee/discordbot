6a751c0c1267f6abf2fe02fc00a81e0e576b80f5
Parshotan Seenanan <parshsee@gmail.com>
2020-06-20 01:21:20 -0400

    updated usage description, sorts by birthday month and day when displaying

--- commands/bdays.js
+++ commands/bdays.js
@@ -1,6 +1,79 @@
 const Birthday = require('../database/models/birthdays');
 const Discord = require('discord.js');
 
+
+async function birthdayDBCall() {
+	// Create a query getting all documents, sorting by firstname THEN lastname
+	// Await the query to the array of document objects
+	const query = Birthday.find().sort({ firstName: 1, lastName: 1 });
+	const doc = await query;
+
+	// Get current date
+	const currentDate = new Date();
+	currentDate.setHours(0, 0, 0, 0);
+	// Initalize arrays for dates before and after current day
+	const afterCurrDate = [];
+	const beforeCurrDate = [];
+
+	// For each document check if its before or after current day
+	doc.forEach(function(birthday) {
+		// Check if the month is ahead
+		if (birthday.birthday.getMonth() + 1 > currentDate.getMonth() + 1) {
+			// Add to array of dates AFTER current day
+			afterCurrDate.push({
+				bday: birthday.birthday,
+				fullName: `${birthday.firstName} ${birthday.lastName}`,
+			});
+			// If the month is the same, check if the date is ahead
+		} else if (birthday.birthday.getMonth() + 1 === currentDate.getMonth() + 1 && birthday.birthday.getDate() > currentDate.getDate()) {
+			// Add to array of dates AFTER current day
+			afterCurrDate.push({
+				bday: birthday.birthday,
+				fullName: `${birthday.firstName} ${birthday.lastName}`,
+			});
+			// Else the month/day is in the past
+		} else {
+			// Add to array of dates BEFORE current day
+			beforeCurrDate.push({
+				bday: birthday.birthday,
+				fullName: `${birthday.firstName} ${birthday.lastName}`,
+			});
+		}
+	});
+
+	// Return an array of arrays
+	return [afterCurrDate, beforeCurrDate];
+}
+
+
+function sortArr(arr, currentDate) {
+	// Get the current month
+	const currMonth = currentDate.getMonth() + 1;
+
+	// Sort the array using sort method
+	// Returning -1 to sort, sorts a to an index lower than b (i.e. a comes first)
+	// Returning 0 to sort, leave a and b unchanged with respect to each other, but sorted with respect to all different elements
+	// Returning 1 to sort, sorts b to an index lower than a (i.e. b comes first)
+	arr.sort((a, b) => {
+		// Get the birthday month and day of the SECOND value (idk why is does second value as a)
+		const bdayMonth = a.bday.getMonth() + 1;
+		const bdayDay = a.bday.getDate();
+
+		// Get the birthday month and day of the FIRST value
+		const bdayMonth2 = b.bday.getMonth() + 1;
+		const bdayDay2 = b.bday.getDate();
+
+		// If the second month is less than the first month swap them
+		if(bdayMonth - currMonth < bdayMonth2 - currMonth) {
+			return -1;
+		} else if(bdayMonth - currMonth === bdayMonth2 - currMonth && bdayDay < bdayDay2) {
+			return -1;
+		}
+	});
+	return arr;
+}
+
+
 function chunkSubstr(str, size) {
 	// Gets the number of chunks (pretty much the amount of messages that will be sent)
 	const numChunks = Math.ceil(str.length / size);
@@ -91,22 +164,37 @@ module.exports = {
 		let reply = '';
 		// If no arguments are given (i.e just ia!bdays)
 		if(!args.length) {
-			// Create a query getting all documents, sorting by firstname THEN lastname
-			// Await the query to the array of document objects
-			const query = Birthday.find().sort({ firstName: 1, lastName: 1 });
-			const doc = await query;
+			// Get current date
+			const currentDate = new Date();
+			currentDate.setTime(0, 0, 0, 0);
+			// Get two arrays,
+			// One containing dates after current day
+			// One containing dates before current day
+			const [ afterCurrDate, beforeCurrDate ] = await birthdayDBCall();
 
-			// For each document format and add to the reply
-			doc.forEach(function(birthday) {
+			// Get the sorted arrays
+			const sortedAfterDates = sortArr(afterCurrDate, currentDate);
+			// For each object in the array, format the date and add to reply
+			sortedAfterDates.forEach(birthday => {
 				// Format the date to mm/dd/yyyy
-				const date = `${birthday.birthday.getMonth() + 1}/${birthday.birthday.getDate()}/${birthday.birthday.getFullYear()}`;
+				const date = `${birthday.bday.getMonth() + 1}/${birthday.bday.getDate()}/${birthday.bday.getFullYear()}`;
 				// Add to the reply
-				reply += `:birthday: **${birthday.firstName} ${birthday.lastName}** \n Birthday: ${date} \n\n`;
+				reply += `:birthday: **${birthday.fullName}** \n Birthday: ${date} \n\n`;
+			});
+
+			// Get the sorted arrays
+			const sortedBeforeDates = sortArr(beforeCurrDate, currentDate);
+			// For each object in the array, format the date and add to reply
+			sortedBeforeDates.forEach(birthday => {
+				// Format the date to mm/dd/yyyy
+				const date = `${birthday.bday.getMonth() + 1}/${birthday.bday.getDate()}/${birthday.bday.getFullYear()}`;
+				// Add to the reply
+				reply += `:birthday: **${birthday.fullName}** \n Birthday: ${date} \n\n`;
 			});
 
 			// If the reply is empty, return error message
 			if(reply === '') {
-				reply += 'No birthdays in database.\nTo add a birthday use ia!bday add [first name] [last name] [birthday (mm/dd/yyyy)]\n\n';
+				reply += 'No birthdays in database.\nTo add a birthday use ia!bday add [first name] [last name] [mm/dd/yyyy]\n\n';
 			}
 			// Create and send embed
 			return sendEmbeds(reply, message);
