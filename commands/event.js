76ffff8a3951c4c6d0bb6c1f1a4ce920ffd35863
Parshotan Seenanan <parshsee@gmail.com>
2020-07-09 10:39:23 -0400

    Setup for command

--- commands/event.js
+++ commands/event.js
@@ -1,6 +1,12 @@
 const Event = require('../database/models/events');
 
+async function addEvent(message, args) {
 
+}
+
+async function removeEvent(message, args) {
+
+}
 
 module.exports = {
 	name: 'event',
@@ -9,7 +15,27 @@ module.exports = {
 	args: true,
 	usage: '\n[add] [event name] **OR** \n[remove] [ID]',
 	execute(message, args) {
+		// Get the first argument and remove it from array
+		const firstArg = args.shift().toLowerCase();
 
-
+		if(firstArg === 'add') {
+			// Shift removes the first arg from array
+			// Message sends total number of args needed (add + 1 args)
+			if(args.length < 1) {
+				return message.channel.send('Command needs at least two (2) arguments, run help command for more info');
+			}
+			// Call addEvent function to add to database
+			addEvent(message, args);
+		} else if(firstArg === 'remove') {
+			// Shift removes the first arg from array
+			// Message sends total number of args needed (remove + 1 args)
+			if(args.length !== 1) {
+				return message.channel.send('Command needs two (2) arguments, run help command for more info');
+			}
+			// Call removeEvent function to remove from database
+			removeEvent(message, args);
+		} else {
+			return message.channel.send('Add or Remove not found. Use ia!help [command] for proper usage');
+		}
 	},
 };
