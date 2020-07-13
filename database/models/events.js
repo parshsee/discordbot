f26bbe9348ddeced7f5645c3edf8409c1afc960d
Parshotan Seenanan <parshsee@gmail.com>
2020-07-13 16:43:36 -0400

    Added id field for user removal

--- database/models/events.js
+++ database/models/events.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 // Create a schema (template) for the event document (table)
 // Array ensures that it is stored in an array, could be optional if no other people join
 const eventSchema = mongoose.Schema({
+	eventId: { type: Number, requried: true },
 	eventName: { type: String, required: true },
 	eventDate: { type: Date, required: true },
 	eventAuthor: { type: String, required: true },
