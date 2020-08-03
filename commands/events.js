4f2a7fc745b0c1b3443401910cc131d23c37bf72
Parshotan Seenanan <parshsee@gmail.com>
2020-08-03 15:26:33 -0400

    Check user argument and perform function

--- commands/events.js
+++ commands/events.js
@@ -31,12 +31,12 @@ function createEmbeddedColumns(message, doc, embed) {
 		if (index % 8 === 0) {
 			embed.addField('ID', `${event.eventId}`, true);
 			embed.addField('Event', `${event.eventName}`, true);
-			embed.addField('Date', `${event.eventTime}`, true);
+			embed.addField('Date', `${event.eventDate.toLocaleString()}`, true);
 			// Else its not the first row, titles can be blank
 		} else {
 			embed.addField('\u200b', `${event.eventId}`, true);
 			embed.addField('\u200b', `${event.eventName}`, true);
-			embed.addField('\u200b', `${event.eventTime}`, true);
+			embed.addField('\u200b', `${event.eventDate.toLocaleString()}`, true);
 		}
 	});
 	// Return the remaining embed after it exits for loop
@@ -51,8 +51,7 @@ module.exports = {
 	description: 'Shows a list of all the events or info about a specific one',
 	args: false,
 	usage: ' **OR** \nia!events [event name]',
-	execute(message, args) {
-		console.log(message, args);
+	async execute(message, args) {
 		// Create the MessageEmbed
 		const embed = new Discord.MessageEmbed()
 			.setColor('#0099ff')
@@ -71,10 +70,51 @@ module.exports = {
 
 		// If no args given (i.e just ia!events)
 		if(!args.length) {
+			// Create a query getting all docs sorting by id
+			// Await the query to get the array of docs
+			const query = Event.find().sort({ id: 1 });
+			const doc = await query;
+			// If there are no events in database, send error message
+			if(!doc) return message.channel.send('No events in database.\nTo add an event use ia!event add [event name]');
 
-		// Else if there is only one argument (should be the ID)
-		} else if(args.length === 1) {
+			// Call function to create and send 3 column embedded
+			createEmbeddedColumns(message, doc, embed);
 
+		// Else if the first arg is a number (integer)
+		} else if(Number(args[0])) {
+			// Store the number in a variable
+			const idNumber = Number(args[0]);
+
+			// Create a query gettingthe id that equals the user given id
+			// Await the query to get an array of docs (even though it will only have one entry)
+			const query = Event.find({ eventId: idNumber });
+			const doc = await query;
+
+			// If there are no events in database, send error message
+			if(!doc) return message.channel.send('Could not find ID in database.\nUse ia!events to see all events');
+
+			// Get the fields from the document (first instance since it's an array)
+			const { eventName, eventDate, eventAuthor } = doc[0];
+			let { eventPeople } = doc[0];
+
+			// If there's only 1 element in the array and it's 'none', remove it
+			if(eventPeople.length === 1 && eventPeople[0] === 'none') eventPeople.pop();
+			// Add the author ID to the beginning of the array
+			eventPeople.unshift(eventAuthor);
+			// Mutate (modify) the array, changing each ID (person) to allow Discord to @ them
+			eventPeople = eventPeople.map((person) => {
+				person = `<@${person}>`;
+				return person;
+			});
+
+			// Add fields and title to embedded
+			embed
+				.addField('Event', `${eventName}`, true)
+				.addField('Participants', `${eventPeople}`, true)
+				.addField('Date', `${eventDate.toLocaleString()}`, true);
+
+			// Send the embedded
+			return message.channel.send(embed);
 		} else {
 			return message.channel.send('Incorrect command usage. For proper usage use ia!help events');
 		}
