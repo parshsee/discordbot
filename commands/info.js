118899443eaa1249521ec2be4c249ea4d7fb0886
Parshotan Seenanan <parshsee@gmail.com>
2020-08-25 08:23:25 -0400

    Reduced to one API call, command usable anywhere

--- commands/info.js
+++ commands/info.js
@@ -15,13 +15,13 @@ async function apiCalls(gameName) {
 			'Accept': 'application/json',
 			'user-key': process.env.API_KEY,
 		},
-		// Search for the game name, return info on cover, game_modes, summary, name of EACH game,
+		// Search for the game name, return info on cover (specifically url), game_modes (specifically name), summary, name of EACH game,
+		// Cover.url removes call to game cover endpoint to retrieve url
+		// Game_modes.name removes call to game modes endpoint to retrieve each game mode
 		// Make sure the cover isn't null (usually indicates game infomration is missing)
-		data: `search "${gameName}"; fields cover, game_modes, name, summary; where cover != null;`,
+		data: `search "${gameName}"; fields cover.url, game_modes.name, name, summary, release_dates; where cover != null;`,
 	// Data without the [0] because it is an array of search ids
 	})).data;
-
-	console.log(searchResult);
 
 	// Go through each object of games
 	// Check if the search result game name is the same as the user game name
@@ -32,7 +32,6 @@ async function apiCalls(gameName) {
 		}
 	});
 
-	console.log(gameInformation);
 	// If the search has no results (empty array) || If gameInformation object has no keys (empty object)
 	// Return error and errorMessage
 	if(!searchResult.length || !Object.keys(gameInformation).length) {
@@ -41,104 +40,17 @@ async function apiCalls(gameName) {
 		return gameInformation;
 	}
 
-	console.log(gameInformation.game_modes);
-
-	// API call to get the game modes info
-	// based on game mode ids
-	const gameModesInfo = (await axios({
-		url: process.env.GAME_MODE_API,
-		method: 'POST',
-		headers: {
-			'Accept': 'application/json',
-			'user-key': process.env.API_KEY,
-		},
-		// Return the name of any game mode where the ID is any of the given ids in the array
-		// Returns an array of objects with gamemode names
-		data: `fields name; where id = (${gameInformation.game_modes});`,
-	})).data;
-
-	console.log(gameModesInfo);
-
 	// Go through each object (mode) in array with the index
 	// Add that mode to the array of game_modes in gameInfomration
 	// Essentially updates the arrays from the IDs to the actual modes
-	gameModesInfo.forEach((mode, index) => {
+	gameInformation.game_modes.forEach((mode, index) => {
 		gameInformation.game_modes[index] = mode.name;
 	});
 
-	console.log(gameInformation);
+	// Format game cover to proper URL
+	gameInformation.cover = `https:${gameInformation.cover.url}`;
 
-	// // API call to get game information
-	// // based on first search result
-	// const gameInfo = (await axios({
-	// 	url: process.env.GAME_API,
-	// 	method: 'POST',
-	// 	headers: {
-	// 		'Accept': 'application/json',
-	// 		'user-key': process.env.API_KEY,
-	// 	},
-	// 	data: `fields cover, game_modes, name, summary; where id = ${firstGameID};`,
-	// // Data with the [0] because it only has one object in the array
-	// })).data[0];
-
-	// console.log(gameInfo);
-
-	// // Get and store the games name and summary in gameInformation object
-	// // Initiates the gameModes as an array
-	// const { name, summary } = gameInfo;
-	// gameInformation = { name, summary };
-	// gameInformation.gameModes = [];
-
-	// // If the game doesn't have any results
-	// // Return error and errorMessage
-	// if(!gameInfo.cover) {
-	// 	console.log('Second error');
-	// 	gameInformation.error = true;
-	// 	gameInformation.errorMessage = 'Search Result Failed';
-	// 	return gameInformation;
-	// }
-
-	// // Get game cover id, used to make api call for cover url
-	// const gameCover = gameInfo.cover;
-
-	// // API call to get the game cover info
-	// // based on cover id
-	// const gameCoverInfo = (await axios({
-	// 	url: process.env.GAME_COVER_API,
-	// 	method: 'POST',
-	// 	headers: {
-	// 		'Accept': 'application/json',
-	// 		'user-key': process.env.API_KEY,
-	// 	},
-	// 	data: `fields url; where id = ${gameCover};`,
-	// // Data with the [0] because it only has one object in the array
-	// })).data[0];
-
-	// // Stores the game cover url in gameInformation object
-	// gameInformation.coverImage = `https:${gameCoverInfo.url}`;
-
-	// // Loop through all the game modes from the gameInfo API call
-	// // INEFFIECENT, Better way?
-	// for(const gameMode of gameInfo.game_modes) {
-
-	// // API call to get the game modes info
-	// // based on game mode ids
-	// const gameModesInfo = (await axios({
-	// 	url: process.env.GAME_MODE_API,
-	// 	method: 'POST',
-	// 	headers: {
-	// 		'Accept': 'application/json',
-	// 		'user-key': process.env.API_KEY,
-	// 	},
-	// 	data: `fields name; where id = ${gameMode};`,
-	// 	// Data with the [0] because it only has one object in the array
-	// })).data[0];
-
-	// 	// Add the game mode name to the array of game modes in gameInformation
-	// 	gameInformation.gameModes.push(gameModesInfo.name);
-	// }
-
-	// return gameInformation;
+	return gameInformation;
 }
 
 module.exports = {
@@ -149,11 +61,9 @@ module.exports = {
 	usage: '[game name]',
 	async execute(message, args) {
 
-		// If not in the freebies channel
+		// If used as a DM
 		// Return error message
-		if(message.channel.name !== 'freebies') {
-			return message.channel.send('This command can only be used in \'freebies\' channel');
-		}
+		if(message.channel.type === 'dm') return message.channel.send('This command can\'t be used as a DM');
 
 		// Get the game name from the args
 		// Make all relevant api calls from game name
@@ -182,11 +92,11 @@ module.exports = {
 			.setTimestamp()
 			.setFooter('Parshotan Seenanan')
 			.setAuthor(message.guild.name, message.guild.iconURL())
-			.setThumbnail(gameInformation.coverImage)
+			.setThumbnail(gameInformation.cover)
 			.setTitle(gameInformation.name)
 			.addFields(
 				{ name: 'Game Information', value: gameSummary, inline: true },
-				{ name: 'Game Modes', value: gameInformation.gameModes, inline: true },
+				{ name: 'Game Modes', value: gameInformation.game_modes, inline: true },
 			);
 		// Return the embed
 		message.channel.send(embed);
