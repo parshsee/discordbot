cd8b5799b2f26cb38a723c968b240782f76f2a66
Parshotan Seenanan <parshsee@gmail.com>
2020-08-26 08:30:41 -0400

    Added try/catch for error handling

--- commands/info.js
+++ commands/info.js
@@ -5,52 +5,60 @@ const discord = require('discord.js');
 async function apiCalls(gameName) {
 	let gameInformation = {};
 
-	// API call to search for game
-	// based on user input
-	// Returns array of objects, each object is a game with similar name
-	const searchResult = (await axios({
-		url: process.env.GAME_API,
-		method: 'POST',
-		headers: {
-			'Accept': 'application/json',
-			'user-key': process.env.API_KEY,
-		},
-		// Search for the game name, return info on cover (specifically url), game_modes (specifically name), summary, name of EACH game,
-		// Cover.url removes call to game cover endpoint to retrieve url
-		// Game_modes.name removes call to game modes endpoint to retrieve each game mode
-		// Make sure the cover isn't null (usually indicates game infomration is missing)
-		data: `search "${gameName}"; fields cover.url, game_modes.name, name, summary, release_dates; where cover != null;`,
-	// Data without the [0] because it is an array of search ids
-	})).data;
+	try {
+		// API call to search for game
+		// based on user input
+		// Returns array of objects, each object is a game with similar name
+		const searchResult = (await axios({
+			url: process.env.GAME_API,
+			method: 'POST',
+			headers: {
+				'Accept': 'application/json',
+				'user-key': process.env.API_KEY,
+			},
+			// Search for the game name, return info on cover (specifically url), game_modes (specifically name), summary, name of EACH game,
+			// Cover.url removes call to game cover endpoint to retrieve url
+			// Game_modes.name removes call to game modes endpoint to retrieve each game mode
+			// Make sure the cover isn't null (usually indicates game infomration is missing)
+			data: `search "${gameName}"; fields cover.url, game_modes.name, name, summary, release_dates; where cover != null;`,
+			// Data without the [0] because it is an array of search ids
+		})).data;
 
-	// Go through each object of games
-	// Check if the search result game name is the same as the user game name
-	// If true, add to gameInformation object
-	searchResult.forEach(result => {
-		if(result.name.toLowerCase() === gameName) {
-			gameInformation = result;
+		// Go through each object of games
+		// Check if the search result game name is the same as the user game name
+		// If true, add to gameInformation object
+		searchResult.forEach(result => {
+			if (result.name.toLowerCase() === gameName) {
+				gameInformation = result;
+			}
+		});
+
+		// If the search has no results (empty array) || If gameInformation object has no keys (empty object)
+		// Return error and errorMessage
+		if (!searchResult.length || !Object.keys(gameInformation).length) {
+			gameInformation.error = true;
+			gameInformation.errorMessage = 'Search Result Failed: Game not in Database';
+			return gameInformation;
 		}
-	});
 
-	// If the search has no results (empty array) || If gameInformation object has no keys (empty object)
-	// Return error and errorMessage
-	if(!searchResult.length || !Object.keys(gameInformation).length) {
+		// Go through each object (mode) in array with the index
+		// Add that mode to the array of game_modes in gameInfomration
+		// Essentially updates the arrays from the IDs to the actual modes
+		gameInformation.game_modes.forEach((mode, index) => {
+			gameInformation.game_modes[index] = mode.name;
+		});
+
+		// Format game cover to proper URL
+		gameInformation.cover = `https:${gameInformation.cover.url}`;
+
+		return gameInformation;
+	} catch (error) {
+		console.log(error);
 		gameInformation.error = true;
-		gameInformation.errorMessage = 'Search Result Failed: Game not in Database';
+		gameInformation.errorMessage = 'Search Result Failed: Error connecting to Database';
 		return gameInformation;
 	}
 
-	// Go through each object (mode) in array with the index
-	// Add that mode to the array of game_modes in gameInfomration
-	// Essentially updates the arrays from the IDs to the actual modes
-	gameInformation.game_modes.forEach((mode, index) => {
-		gameInformation.game_modes[index] = mode.name;
-	});
-
-	// Format game cover to proper URL
-	gameInformation.cover = `https:${gameInformation.cover.url}`;
-
-	return gameInformation;
 }
 
 module.exports = {
