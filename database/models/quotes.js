4b8741c820123a10947e67b3abdd0b2678305c8c
Parshotan Seenanan <parshsee@gmail.com>
2020-10-15 13:58:15 -0400

    Updated models

--- database/models/quotes.js
+++ database/models/quotes.js
@@ -3,11 +3,11 @@ const mongoose = require('mongoose');
 // Create a schema (template) for the quotes document (table)
 // Default ensures that if blank, inserts a value automatically
 const quotesSchema = mongoose.Schema({
-	id: { type: Number, require: true },
-	firstName: { type: String, require: true },
-	lastName: { type: String, require: true },
-	quote: { type: String, require: true },
-	timestamp: { type: Date, require: true, default: Date.now },
+	id: { type: Number, required: true },
+	firstName: { type: String, required: true },
+	lastName: { type: String, required: true },
+	quote: { type: String, requirequiredre: true },
+	timestamp: { type: Date, required: true, default: Date.now },
 });
 
 // Export the model as 'Quotes' to be used when adding
