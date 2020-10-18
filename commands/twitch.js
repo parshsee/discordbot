09ea27bd537efd20d0604415b182f7768653f628
Parshotan Seenanan <parshsee@gmail.com>
2020-10-17 22:53:35 -0400

    Adds streamer to database if they exist

--- commands/twitch.js
+++ commands/twitch.js
@@ -1,9 +1,55 @@
 require('dotenv').config();
 const axios = require('axios');
+const Streamer = require('../database/models/streamers');
 
 
 async function addStreamer(message, args) {
+	const twitchStreamer = args[0];
 
+	try {
+		const searchResult = (await axios({
+			url: `${process.env.TWITCH_USER_API}`,
+			method: 'GET',
+			headers: {
+				'Accept': 'application/json',
+				'Client-ID': process.env.TWITCH_CLIENT_ID,
+				'Authorization': `Bearer ${process.env.TWITCH_TOKEN}`,
+			},
+			params: {
+				login: twitchStreamer,
+			},
+		})).data.data;
+
+		if(searchResult.length) {
+			const idNumber = await Streamer.countDocuments() + 1;
+			console.log(idNumber);
+
+			// Construct a new streamer document from the model
+			const streamer = new Streamer({
+				id: idNumber,
+				streamerName: twitchStreamer,
+				gameTitle: '',
+				status: 'Offline',
+			});
+
+			// Save the Streamer to the database
+			(async () => {
+				try {
+					await streamer.save();
+					console.log('Streamer added to Database');
+					return message.channel.send('Streamer Added Successfully');
+				} catch (err) {
+					console.log('error: ' + err);
+					return message.channel.send('Error saving streamer');
+				}
+			})();
+		} else {
+			message.channel.send('Streamer not found');
+		}
+		console.log(searchResult);
+	} catch(err) {
+		console.log(err);
+	}
 }
 
 async function removeStreamer(message, args) {
@@ -45,6 +91,8 @@ module.exports = {
 		} else if(firstArg === 'list') {
 			// ID	|	Twitch Streamer Name   |   Twitch URL
 			await listStreamer(message, args);
+		} else {
+			return message.channel.send('Add or Remove not found. Use ia!help [twitch] for proper usage');
 		}
 	},
 };
