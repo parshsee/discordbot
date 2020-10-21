985e9c0e0aa5ff3e9bfed496d33c79120f4bd01c
Parshotan Seenanan <parshsee@gmail.com>
2020-10-21 08:58:01 -0400

    Added functionality to list streamers

--- commands/twitch.js
+++ commands/twitch.js
@@ -93,8 +93,58 @@ async function updateCollectionIDs() {
 	}
 }
 
-async function listStreamer(message, args) {
+async function listStreamer(message) {
+	// Create the MessageEmbed
+	const embed = new Discord.MessageEmbed()
+		.setColor('#0099ff')
+		.setTimestamp()
+		.setFooter('Parshotan Seenanan')
+		.setAuthor(message.guild.name, message.guild.iconURL())
+		.setThumbnail(message.guild.iconURL());
 
+	const doc = await Streamer.find().sort({ id: 1 });
+	console.log('Streamer DB Called');
+
+	if(!doc.length) return message.channel.send('No streamers in database.\nTo add a streamer use \'ia!twitch add [streamer name]\'');
+
+	// The limit of how many stremers can be in an embed
+	// Only have 25 fields
+	// Need to set the ID, Streamer, and Twitch Url each column is a different field (so 3)
+	// 8 * 3 = 24, Only 8 events can be in an embed at a time
+	let limit = 8;
+
+	// Loop through the array of docs getting the streamer object and index
+	doc.forEach((streamer, index) => {
+		// If the index = the limit
+		if (index === limit) {
+			// Ternary Operator, set initial title for first embed and the titles for the others
+			embed.setTitle(index === 8 ? 'All Streamers' : 'All Streamers Cont.');
+			// Increase the limit
+			limit += 8;
+			// Wait for the embed to be send
+			// forEach function doesn't need it to be await for some reason
+			message.channel.send({ embed });
+			// Clear all fields from the embed
+			// Allows me to add another 25 fields
+			embed.fields = [];
+		}
+
+		// If the remainder is 0, indicates that this will be the first row in embed, set titles
+		if (index % 8 === 0) {
+			embed.addField('ID', `${streamer.id}`, true);
+			embed.addField('Streamer', `${streamer.streamerName}`, true);
+			embed.addField('Twitch URL', `[twitch.tv/${streamer.streamerName}](https://twitch.tv/${streamer.streamerName})`, true);
+			// Else its not the first row, titles can be blank
+		} else {
+			embed.addField('\u200b', `${streamer.id}`, true);
+			embed.addField('\u200b', `${streamer.streamerName}`, true);
+			embed.addField('\u200b', `[twitch.tv/${streamer.streamerName}](https://twitch.tv/${streamer.streamerName})`, true);
+		}
+	});
+	// Return the remaining embed after it exits for loop
+	// Ensures that the last events are sent
+	// I.e if 28 events in db, 24 will get sent with code above, last 4 will get sent with this
+	return message.channel.send(embed);
 }
 
 module.exports = {
@@ -127,9 +177,9 @@ module.exports = {
 			await removeStreamer(message, args);
 		} else if(firstArg === 'list') {
 			// ID	|	Twitch Streamer Name   |   Twitch URL
-			await listStreamer(message, args);
+			await listStreamer(message);
 		} else {
-			return message.channel.send('Add or Remove not found. Use ia!help [twitch] for proper usage');
+			return message.channel.send('Add or Remove not found. Use \'ia!help twitch\' for proper usage');
 		}
 	},
 };
