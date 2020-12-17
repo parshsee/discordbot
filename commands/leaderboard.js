1569438bcb14354706ab314d920ef7abbd4f5ccc
Parshotan Seenanan <parshsee@gmail.com>
2020-12-17 00:29:34 -0500

    Added function to add and remove leaderboards

--- commands/leaderboard.js
+++ commands/leaderboard.js
@@ -1,11 +1,77 @@
 const Leaderboard = require('../database/models/leaderboards');
 
 async function addLeaderboard(message, args) {
+	// Get the full tournament name
 	const tournamentName = args.join(' ');
+	// Create a tournament object to store in DB
+	const tournamentObject = {
+		name: tournamentName,
+		players: {},
+	};
+	// Get all documents in DB sorting by id
+	const query = await Leaderboard.find().sort({ id: 1 });
+	// Check if the array is empty (meaning nothing in db)
+	// True = Get the id of the last entry and add 1
+	// False = Set the id to 1
+	const idNumber = query.length ? query[query.length - 1].id + 1 : 1;
+
+	// Construct a new Leaderboard document from the model
+	const leaderboard = new Leaderboard({
+		id: idNumber,
+		leaderboard: tournamentObject,
+	});
+
+	// Save the Leaderboard to the database
+	(async () => {
+		try {
+			await leaderboard.save();
+			console.log('Leaderboard added to Database');
+			return message.channel.send('Leaderboard Added Successfully');
+		} catch (err) {
+			console.log('error: ' + err);
+			return message.channel.send('Error saving leaderboard.');
+		}
+	})();
 }
 
 async function endLeaderboard(message, args) {
-	
+	// Check if given a tournament ID or name
+	if(args.length === 1 && Number(args[0])) {
+		const tournamentId = args[0];
+		// Create a query to find and delete the tournament based on tournamentId
+		// Returns an array of docs (even though if found it will only have 1 doc)
+		const query = await Leaderboard.findOneAndDelete({ id: tournamentId });
+		// If there are no tournaments in database, send error message
+		if(!query) return message.channel.send('Could not find ID in database.\nUse \'ia!leaderboard list\' to see all tournaments');
+
+		console.log(query);
+		// Destructured tournament name from query
+		const { name } = query.leaderboard;
+
+		await updateCollectionIDs();
+
+		console.log('Leaderboard removed from Database');
+		// Return a message saying deletion was successful
+		return message.channel.send(`${name} has ended.`);
+	} else {
+		const tournamentName = args.join(' ');
+		// Create a query to find and delete the tournament based on tournamentName
+		// Returns an array of docs (even though if found it will only have 1 doc)
+		// Searching through an object is done with quotation marks
+		const query = await Leaderboard.findOneAndDelete({ 'leaderboard.name': tournamentName });
+		// If there are no tournaments in database, send error message
+		if(!query) return message.channel.send('Could not find ID in database.\nUse \'ia!leaderboard list\' to see all tournaments.\n Make sure it is spelt exactly the same or use the ID instead');
+
+		console.log(query);
+		// Destructured tournament name from query
+		const { name } = query.leaderboard;
+
+		await updateCollectionIDs();
+
+		console.log('Leaderboard removed from Database');
+		// Return a message saying deletion was successful
+		return message.channel.send(`${name} has ended.`);
+	}
 }
 
 async function addPlayer(message, args) {
@@ -17,6 +83,28 @@ async function removePlayer(message, args) {
 }
 
 async function updateScores(message, args) {
+
+}
+
+// After removing a tournament, go through the collection
+// Update all ids to be in order
+// Solves issue of having ids [1, 2, 3, 4] deleting id 3, and now ids show as [1, 2, 4]
+async function updateCollectionIDs() {
+	// Get number of documents in collection
+	const numberOfDocs = await Leaderboard.countDocuments();
+
+	for(let i = 0; i < numberOfDocs; i++) {
+		// Find all documents matching the condition (id > i)
+		// Update the first documents id to be i + 1
+		// Function takes a filter, an update, and a callback
+		Leaderboard.updateOne(
+			{ id: { $gt:i } },
+			{ id: i + 1 },
+			(err) => {
+				if (err) console.log(err);
+			});
+	}
+
 
 }
 
